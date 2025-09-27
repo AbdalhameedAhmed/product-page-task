f34b3ae6377c1db8e380133f01ba8689c584fdbf
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-09-27 12:05:56 +0300

    feat: Add cart menu styles on mobile

--- src/components/Header/Cart.tsx
+++ src/components/Header/Cart.tsx
@@ -30,7 +30,7 @@ export default function Cart() {
       <div
         onClick={(e) => e.stopPropagation()}
         className={clsx(
-          "absolute -right-4 z-30 w-[400px] origin-[90%_0%] scale-0 space-y-2 rounded-lg border border-gray-200 bg-white px-4 py-6 shadow-xl transition-all duration-300",
+          "fixed left-1/2 z-30 w-[90%] origin-[90%_0%] -translate-x-1/2 scale-0 space-y-2 rounded-lg border border-gray-200 bg-white px-4 py-6 shadow-xl transition-all duration-300 sm:absolute sm:-right-4 sm:left-auto sm:w-[400px] sm:translate-x-0",
           {
             "scale-100": isOpen,
           },
@@ -45,7 +45,7 @@ export default function Cart() {
                 <img src={item.image} alt="product image" className="w-12" />
                 <div className="flex flex-col gap-1">
                   <p className="text-sm">{item.name}</p>
-                  <p>{item.price}</p>
+                  <p className="text-sm">{item.price}</p>
                 </div>
               </div>
               <div className="flex items-center gap-8">
